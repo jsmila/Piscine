12dad6215947a9f57072b4d2a39f099122effe49
Bibou <bibou@Bibous-Mac.local>
2017-09-10 05:26:36 -0700

    finalisation atoi

--- j03/ex08/ft_atoi.c
+++ j03/ex08/ft_atoi.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/*
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while(*str)
+		ft_putchar(*str++);
+}
+*/
 
 int	ft_atoi(char *str)
 {
@@ -8,30 +22,34 @@ int	ft_atoi(char *str)
 
 	i = 0;
 	result = 0;
-	while (str[i] != '\0')
+	if (*str)
 	{
-		while (str[i] <= 32)
-			i++;
-		if (str[i] == '-')
-			{
-			signe = -1;
-			i++;
-			}
-		else
-			signe = 1;
-		while (str[i] >= '0' && str[i] <= '9')
+		while (str[i] != '\0')
 		{
-			result *= 10;
-			result += str[i] - '0';
-			i++;	
+			while (str[i] <= 32)
+				i++;
+			if (str[i] == '-')
+				{
+				signe = -1;
+				i++;
+				}
+			else
+				signe = 1;
+			while (str[i] >= '0' && str[i] <= '9')
+			{
+				result *= 10;
+				result += str[i] - '0';
+				i++;	
+			}
+			return (result * signe);	
 		}
-		return (result * signe);	
 	}
 	return (0);
 }
 
 int	main(void)
-{	
-	printf("%d\n", ft_atoi("-123456789"));
+{
+	printf("%d\n", 2147483647 * 2);
+	printf("%d\n", ft_atoi("12345678901"));
 	return (0);
 }
